f001b96f1a46190b8161dca9fda146832eec66d9
efraimverdikas <verdikaefraim@gmail.com>
2021-09-23 15:58:08 +0700

    projek2

---
+++ pustaka-booking/application/controllers/Welcome.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller 
+{
+	public function index()
+	{
+		
+		echo"<h1>Perkenalkan</h1>";
+		echo"Nama			: Efraim Verdika Sihaloho <br>
+			 Tempat lahir	: Jakarta <br>
+			 Tanggal lahir	: 29 Februari 2000 <br>
+			 Tempat tinggal	: vida bumipala,taman apel merah 5 blk a3/56 <br>
+			 Motivasi kuliah: Alasan utama yaitu untuk mengasah skil dan tujuan utama untuk <br>
+			 				  mendapatkan ijasah agar bisa lolos verifikasi berkas dalam tahapan penerimaan<br> 
+							  lowongan kerja.<br> ";
+
+
+	}
+
+
+
+}
